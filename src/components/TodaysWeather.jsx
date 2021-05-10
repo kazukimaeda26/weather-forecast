fdfb32e50ce95ac2f792eb5f0e2a9d457e65ae3e
kazukimaeda26 <kazukimaeda26@gmail.com>
2021-05-10 15:49:51 +0900

    現在日時の取得表示

--- src/components/TodaysWeather.jsx
+++ src/components/TodaysWeather.jsx
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
 import sample from '../assets/img/sample.png';
 
 const TodaysWeather = () => {
+  let initialCurrentTime = new Date();
+  let year = initialCurrentTime.getUTCFullYear()
+  let month = initialCurrentTime.getUTCMonth() + 1
+  let day = initialCurrentTime.getUTCDate()
+  let wdayNum = initialCurrentTime.getUTCDay()
+  let wdayArray = ["（日）", "（月）", "（火）", "（水）", "（木）", "（金）", "（土）", ]
+  let wday = wdayArray[wdayNum]
+
+  let hour = initialCurrentTime.getUTCHours() + 9
+  let minute = initialCurrentTime.getUTCMinutes()
+  let sec = initialCurrentTime.getSeconds()
+
+  const [currentTime, setCurrentTime] = useState({
+      year: year,
+      month: month,
+      day: day,
+      wday: wday
+    })
+
+    
+
   return(
     <div className="todaysForecastWrapper">
       <div className="todaysDate">
-        XX月XX日 現在時刻
+        {year}年{month}月{day}日{wday} 現在時刻: {hour}時{minute}分{sec}秒
       </div>
-      <div className="presentLocation">現在地</div>
+      <div className="presentLocation">現在地:</div>
       <div className="todaysForecast">
         <img src={sample} alt="画像" className="weatherImg" />
         <p className="todaysTempature">0℃</p>
