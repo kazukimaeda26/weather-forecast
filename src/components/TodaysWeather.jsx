857a2b0d6c1a66a9ba2299a09ebc6dd8a3ae6530
kazukimaeda26 <kazukimaeda26@gmail.com>
2021-05-25 17:04:30 +0900

    fix

--- src/components/TodaysWeather.jsx
+++ src/components/TodaysWeather.jsx
@@ -16,6 +16,17 @@ const TodaysWeather = (props) => {
   const pressure = props.todaysWeather.pressure;
   const humidity = props.todaysWeather.humidity;
 
+
+  const defineWindDirection = (degree) => {
+    if( (0 <= degree && degree < 45) || (270 <= degree && degree < 360)) var windDirection = "北寄りの風";
+    else if( (45 <= degree && degree < 135)) var windDirection="東寄りの風";
+    else if( (135 <= degree && degree < 225)) var windDirection="南寄りの風";
+    else if( (225 <= degree && degree < 315)) var windDirection="西よりの風"
+    
+    return windDirection;
+  }
+
+
   return(
     <div className="todaysForecastWrapper">
       <TodaysDate currentTime={props.currentTime}/>
@@ -32,7 +43,7 @@ const TodaysWeather = (props) => {
           体感温度：{feelsLike}℃　最高気温：{tempMax}℃　最低気温：{tempMin}℃
         </p>
         <p className="todaysDetailBottom">
-          風：{windSpeed}m/s　北寄りの風（要変更）　気圧：{pressure}hPa　湿度：{humidity}%
+          風：{windSpeed}m/s　{defineWindDirection(windDeg)}　気圧：{pressure}hPa　湿度：{humidity}%
         </p>
       </div>
     </div>
