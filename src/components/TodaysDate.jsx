acf56890b53a101b6fbe4850a82f7c1770b72ef7
kazukimaeda26 <kazukimaeda26@gmail.com>
2021-05-25 16:28:41 +0900

    fix

--- src/components/TodaysDate.jsx
+++ src/components/TodaysDate.jsx
@@ -4,7 +4,7 @@ const TodaysDate = (props) => {
 
   return (
     <div className="todaysDate">
-      {props.currentTime.year}年{props.currentTime.month}月{props.currentTime.day}日{props.currentTime.wday} 現在時刻: {props.currentTime.hour}時{props.currentTime.minute}分{props.currentTime.sec}秒
+      {props.currentTime.year}年{props.currentTime.month}月{props.currentTime.day}日{props.currentTime.wday} 現在時刻: {props.currentTime.hour}時{props.currentTime.minute}分
     </div>
   )
 }
